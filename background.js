900a43c8a73e3887dfa6b2f1e84a955057937aa8
Chreznor <chechur@bk.ru>
2017-06-01 04:35:13 +0300

    Started the 1st and 2nd page

---
+++ background.js
@@ -0,0 +1,47 @@
+
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('#top-background-image').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (30 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (20 * lMouseY) / 100;
+
+});
+
+moveBackground();
+
+var tStart = 100 // Start transition 100px from top
+  , tEnd = 500   // End at 500px
+  , cStart = [157, 176, 209]
+  , cEnd = [220, 224, 197]
+  , cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[1] - cStart[0]];
+
+$(document).ready(function(){
+    $(document).scroll(function() {
+        var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); // % of transition
+        p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
+        var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
+        $(".second-page").css('background-color', 'rgb(' + cBg.join(',') +')');
+    });
+});
