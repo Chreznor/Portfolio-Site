54f02d72e85f9284a1b5a1a01e2e130afe472fa1
Chreznor <chechur@bk.ru>
2017-08-22 14:08:49 +0300

    Finally achieved what I wanted in section 1

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// MOVING BACKGROUND ANIMATION
 
 var lFollowX = 0,
     lFollowY = 0,
@@ -31,9 +32,15 @@ $(window).on('mousemove click', function(e) {
 
 moveBackground();
 
+//CHANING BACKGROUND COLOR ON SCROLL ANIMATION
+
+
 function backColorChange() {
-var tStart = 400 // Start transition 100px from top
-  , tEnd = 900   // End at 500px
+  const section1 = document.getElementById('1');
+  const sectionVal1 = section1.getBoundingClientRect();
+  console.log(sectionVal1.top + window.scrollY, sectionVal1.bottom + window.scrollY);
+  var tStart = sectionVal1.top + window.scrollY - sectionVal1.height// Start transition 100px from top 400
+  , tEnd = sectionVal1.top + window.scrollY  // End at 500px 900 
   , cStart = [4, 95, 99]
   , cEnd = [184, 192, 172]
   , cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[2] - cStart[2]];
@@ -41,6 +48,7 @@ var tStart = 400 // Start transition 100px from top
 $(document).ready(function(){
     $(document).scroll(function() {
         var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); // % of transition
+        //console.log($(this).scrollTop(), tEnd - tStart);
         p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
         var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
         $(".second-page").css('background-color', 'rgb(' + cBg.join(',') +')');
@@ -50,28 +58,35 @@ $(document).ready(function(){
 
 backColorChange()
 
-function backColorChange2() {
-var tStart = 1400 // Start transition 100px from top
-  , tEnd = 1900   // End at 500px
-  , cStart = [184, 192, 172]
-  , cEnd = [255, 86, 15]
-  , cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[2] - cStart[2]];
 
-$(document).ready(function(){
-    $(document).scroll(function() {
-        var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); // % of transition
-        p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
-        var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
-        $(".third-page").css('background-color', 'rgb(' + cBg.join(',') +')');
-    });
-});
+function backColorChange2() {
+  const section2 = document.getElementById('2');
+  const sectionVal2 = section2.getBoundingClientRect();
+  console.log(sectionVal2.top + window.scrollY, sectionVal2.bottom + window.scrollY);
+  var tStart = sectionVal2.top + window.scrollY - sectionVal2.height // Start transition 100px from top 1400
+    , tEnd = sectionVal2.top + window.scrollY   // End at 500px 1900
+    , cStart = [184, 192, 172]
+    , cEnd = [255, 86, 15]
+    , cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[2] - cStart[2]];
+
+  $(document).ready(function(){
+      $(document).scroll(function() {
+          var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); // % of transition
+          p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
+          var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
+          $(".third-page").css('background-color', 'rgb(' + cBg.join(',') +')');
+      });
+  });
 };
 
-backColorChange2()
+backColorChange2();
 
 function backColorChange3() {
-var tStart = 2400 // Start transition 100px from top
-  , tEnd = 2900   // End at 500px
+  const section3 = document.getElementById('3');
+  const sectionVal3 = section3.getBoundingClientRect();
+  console.log(sectionVal3.top + window.scrollY, sectionVal3.bottom + window.scrollY);
+  var tStart = sectionVal3.top + window.scrollY - sectionVal3.height// Start transition 100px from top 2400
+  , tEnd = sectionVal3.top + window.scrollY // End at 500px 2900
   , cStart = [255, 86, 15]
   , cEnd = [46, 42, 170]
   , cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[2] - cStart[2]];
@@ -87,6 +102,8 @@ $(document).ready(function(){
 };
 
 backColorChange3()
+
+// SCROLL DOWN ANIMATION
 
 var sections = $('section')
   , nav = $('nav')
@@ -109,10 +126,15 @@ $(window).on('scroll', function () {
   });
 })
 
+//SECTION 1 ANIMATION
+
 const centerContainer = document.querySelector('.center-container');
 const leftSideContainer = document.querySelector('.left-side-container');
 const rightSideContainer = document.querySelector('.right-side-container');
-const wrapper = document.querySelector('.wrapper');
+const borderLeft = document.querySelector('.border-left');
+const borderRight = document.querySelector('.border-right');
+const animEl = [borderLeft, borderRight, centerContainer];
+let animState = false;
 
 function actAnim() {
   rightSideContainer.classList.add('opening-right');
@@ -128,24 +150,28 @@ function deactAnim() {
   rightSideContainer.classList.remove('opening-right');
   leftSideContainer.classList.remove('opening-left');
   centerContainer.classList.remove('center-container-widen');
-  document.querySelector('.border-left').style.backgroundColor = '#b8c0ac';
-  document.querySelector('.border-right').style.backgroundColor = '#b8c0ac';
-  document.querySelector('#word-left').style.color = 'black';
-  document.querySelector('#word-right').style.color = 'black';
+  if (animState === false) {
+    document.querySelector('.border-left').style.backgroundColor = '#b8c0ac';
+    document.querySelector('.border-right').style.backgroundColor = '#b8c0ac';
+    document.querySelector('#word-left').style.color = 'black';
+    document.querySelector('#word-right').style.color = 'black';
+  } else {
+    return;
+  }
+  
 }
 
 function toggleAnim() {
   rightSideContainer.classList.toggle('open-right');
   leftSideContainer.classList.toggle('open-left');
   centerContainer.classList.toggle('center-container-widenned');
-  wrapper.classList.toggle('wrapper-width');
+  [borderLeft, borderRight].forEach(elem => elem.classList.toggle('border-color-toggle'));
+  animState = !animState;
 }
 
-
-wrapper.addEventListener('mouseenter', actAnim);
-wrapper.addEventListener('mouseleave', deactAnim);
-wrapper.addEventListener('click', toggleAnim);
-
+animEl.forEach(elem => elem.addEventListener('mouseenter', actAnim));
+animEl.forEach(elem => elem.addEventListener('mouseleave', deactAnim));
+animEl.forEach(elem => elem.addEventListener('click', toggleAnim));
 
 // $(document).ready(function(){
 //   $(".wrapper").mouseenter(function(){
